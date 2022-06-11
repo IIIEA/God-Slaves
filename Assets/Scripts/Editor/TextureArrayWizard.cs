4cf6cdc6737c6e8314e429d35c4cec8aa518bcc4
IIIEA <egor.solo2000@bk.ru>
2022-06-11 11:47:08 +0300

    1.3

--- Assets/Scripts/Editor/TextureArrayWizard.cs
+++ Assets/Scripts/Editor/TextureArrayWizard.cs
@@ -1,42 +1,44 @@
 ﻿using UnityEditor;
 using UnityEngine;
 
-public class TextureArrayWizard : ScriptableWizard {
+public class TextureArrayWizard : ScriptableWizard
+{
+    public Texture2D[] Textures;
 
-	public Texture2D[] textures;
+    [MenuItem("Assets/Create/Texture Array")]
+    private static void CreateWizard()
+    {
+        DisplayWizard<TextureArrayWizard>("Create Texture Array", "Create");
+    }
 
-	[MenuItem ("Assets/Create/Texture Array")]
-	static void CreateWizard () {
-		ScriptableWizard.DisplayWizard<TextureArrayWizard>(
-			"Create Texture Array", "Create"
-		);
-	}
+    void OnWizardCreate()
+    {
+        if (Textures.Length == 0)
+        {
+            return;
+        }
 
-	void OnWizardCreate () {
-		if (textures.Length == 0) {
-			return;
-		}
-		string path = EditorUtility.SaveFilePanelInProject(
-			"Save Texture Array", "Texture Array", "asset", "Save Texture Array"
-		);
-		if (path.Length == 0) {
-			return;
-		}
+        string path = EditorUtility.SaveFilePanelInProject("Save Texture Array", "Texture Array", "asset", "Save Texture Array");
 
-		Texture2D t = textures[0];
-		Texture2DArray textureArray = new Texture2DArray(
-			t.width, t.height, textures.Length, t.format, t.mipmapCount > 1
-		);
-		textureArray.anisoLevel = t.anisoLevel;
-		textureArray.filterMode = t.filterMode;
-		textureArray.wrapMode = t.wrapMode;
+        if (path.Length == 0)
+        {
+            return;
+        }
 
-		for (int i = 0; i < textures.Length; i++) {
-			for (int m = 0; m < t.mipmapCount; m++) {
-				Graphics.CopyTexture(textures[i], 0, m, textureArray, i, m);
-			}
-		}
+        Texture2D t = Textures[0];
+        Texture2DArray textureArray = new Texture2DArray(t.width, t.height, Textures.Length, t.format, t.mipmapCount > 1);
+        textureArray.anisoLevel = t.anisoLevel;
+        textureArray.filterMode = t.filterMode;
+        textureArray.wrapMode = t.wrapMode;
 
-		AssetDatabase.CreateAsset(textureArray, path);
-	}
+        for (int i = 0; i < Textures.Length; i++)
+        {
+            for (int m = 0; m < t.mipmapCount; m++)
+            {
+                Graphics.CopyTexture(Textures[i], 0, m, textureArray, i, m);
+            }
+        }
+
+        AssetDatabase.CreateAsset(textureArray, path);
+    }
 }
